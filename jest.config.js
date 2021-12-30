699347ccb10d5f1eb382c70bfa38e7bf53fb9ad8
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-30 21:57:18 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  testMatch: [
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+    '<rootDir>/tests/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/src/$1'
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/globals.d.ts',
+    '!<rootDir>/src/__tests__/**',
+    '!<rootDir>/src/__fixtures__/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+}
