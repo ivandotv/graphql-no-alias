f769837b94044452ffabb428fc54e214061fe614
ivandotv <390700+ivandotv@users.noreply.github.com>
2022-01-24 22:24:20 +0100

    fix jest code coverage

---
+++ .changeset/few-rings-invite.md
@@ -0,0 +1,8 @@
+---
+'envelop-no-alias': major
+'graphql-no-alias': patch
+---
+
+Initial publish of `envelop-no-alias`
+
+Update readme for: `graphql-no-alias`
--- packages/envelop/jest.config.js
+++ packages/envelop/jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   testMatch: ['<rootDir>/src/__tests__/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: [
     '<rootDir>/src/**',
-    '!<rootDir>/src/index.ts',
     '!<rootDir>/src/__tests__/**',
     '!<rootDir>/src/__fixtures__/**'
   ]
